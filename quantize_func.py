e22e171f582d894af624a86b11bf1cbae6741b6e
PEIYAN <45463777+PeiyanFlying@users.noreply.github.com>
2024-06-09 12:02:14 -0400

    Add files via upload

--- quantize_func.py
+++ quantize_func.py
@@ -28,15 +28,12 @@ def clamp(input, min, max):
     return torch.clamp(input, min, max)
 
 
-
 def log_shift(value_fp):
     value_shift = 2 ** (torch.log2(value_fp).ceil())
     return value_shift
 def get_quantized_range(num_bits):
     n = 2 ** (num_bits - 1)
     return 0, 2 ** num_bits - 1
-
-
 
 
 def linear_quantize(input, scale_factor):
@@ -51,15 +48,12 @@ def linear_quantize_clamp(input, scale_factor, clamp_min, clamp_max):
     return clamp(output, clamp_min, clamp_max)
 
 
-
-
-
 def trunc(fp_data, nbits=8):
     il = torch.log2(torch.max(fp_data.max(), fp_data.min().abs())) + 1
     il = math.ceil(il - 1e-5)
     qcode = nbits - il
     scale_factor = 2 ** qcode
-    clamp_min, clamp_max = get_quantized_range(nbits, signed=True)
+    clamp_min, clamp_max = get_quantized_range(nbits)
     q_data = linear_quantize_clamp(fp_data, scale_factor, clamp_min, clamp_max)
     q_data = linear_dequantize(q_data, scale_factor)
     return q_data, qcode
@@ -87,7 +81,6 @@ def get_default_kwargs_q(kwargs_q, layer_type):
     return kwargs_q
 
 class StopGrad(torch.autograd.Function):
-
     @staticmethod
     def forward(ctx, weight, stopGradientMask):
         ctx.save_for_backward(stopGradientMask)
@@ -106,22 +99,16 @@ class Quantize_Conv2d(nn.Conv2d):
         super(Quantize_Conv2d, self).__init__(in_channels, out_channels, kernel_size, stride=stride,
                                        padding=padding, dilation=dilation, groups=groups, bias=bias)
         self.kwargs_q = get_default_kwargs_q(kwargs_q, layer_type=self)
-        self.nbits = kwargs_q['nbits']
-        if self.nbits < 0:
-            self.register_parameter('alpha', None)
-            return
         self.q_mode = kwargs_q['mode']
         if self.q_mode == Quantizemode.kernel_wise:
             self.alpha = Parameter(torch.Tensor(out_channels))
         else:  # layer-wise quantization
             self.alpha = Parameter(torch.Tensor(1))
+        self.nbits = kwargs_q['nbits']
+        if self.nbits < 0:
+            self.register_parameter('alpha', None)
+            return
         self.register_buffer('init_state', torch.zeros(1))
-
-    def add_param(self, param_k, param_v):
-        self.kwargs_q[param_k] = param_v
-
-    def set_bit(self, nbits):
-        self.kwargs_q['nbits'] = nbits
 
     def extra_repr(self):
         s_prefix = super(Quantize_Conv2d, self).extra_repr()
@@ -129,19 +116,27 @@ class Quantize_Conv2d(nn.Conv2d):
             return '{}, fake'.format(s_prefix)
         return '{}, {}'.format(s_prefix, self.kwargs_q)
 
+    def add_param(self, param_k, param_v):
+        self.kwargs_q[param_k] = param_v
+
+    def set_bit(self, nbits):
+        self.kwargs_q['nbits'] = nbits
+
+
+
 
 class Quantize_Linear(nn.Linear):
     def __init__(self, in_features, out_features, bias=True, **kwargs_q):
         super(Quantize_Linear, self).__init__(in_features=in_features, out_features=out_features, bias=bias)
         self.kwargs_q = get_default_kwargs_q(kwargs_q, layer_type=self)
+        if self.q_mode == Quantizemode.kernel_wise:
+            self.alpha = Parameter(torch.Tensor(out_features))
+        self.alpha = Parameter(torch.Tensor(1))
         self.nbits = kwargs_q['nbits']
         if self.nbits < 0:
             self.register_parameter('alpha', None)
             return
         self.q_mode = kwargs_q['mode']
-        self.alpha = Parameter(torch.Tensor(1))
-        if self.q_mode == Quantizemode.kernel_wise:
-            self.alpha = Parameter(torch.Tensor(out_features))
         self.register_buffer('init_state', torch.zeros(1))
 
     def add_param(self, param_k, param_v):
@@ -158,31 +153,34 @@ class Quantize_Activation(nn.Module):
     def __init__(self, in_features, **kwargs_q):
         super(Quantize_Activation, self).__init__()
         self.kwargs_q = get_default_kwargs_q(kwargs_q, layer_type=self)
-        self.nbits = kwargs_q['nbits']
-        if self.nbits < 0:
-            self.register_parameter('alpha', None)
-            self.register_parameter('zero_point', None)
-            return
-        # self.signed = kwargs_q['signed']
         self.q_mode = kwargs_q['mode']
-        self.alpha = Parameter(torch.Tensor(1))
+
         self.zero_point = Parameter(torch.Tensor([0]))
         if self.q_mode == Quantizemode.kernel_wise:
             self.alpha = Parameter(torch.Tensor(in_features))
             self.zero_point = Parameter(torch.Tensor(in_features))
             torch.nn.init.zeros_(self.zero_point)
-        # self.zero_point = Parameter(torch.Tensor([0]))
+        self.nbits = kwargs_q['nbits']
+        if self.nbits < 0:
+            self.register_parameter('alpha', None)
+            self.register_parameter('zero_point', None)
+            return
+        self.alpha = Parameter(torch.Tensor(1))
         self.register_buffer('init_state', torch.zeros(1))
         self.register_buffer('signed', torch.zeros(1))
-
-    def add_param(self, param_k, param_v):
-        self.kwargs_q[param_k] = param_v
-
-    def set_bit(self, nbits):
-        self.kwargs_q['nbits'] = nbits
 
     def extra_repr(self):
         # s_prefix = super(Quantize_Activation, self).extra_repr()
         if self.alpha is None:
             return 'fake'
         return '{}'.format(self.kwargs_q)
+    
+    def set_bit(self, nbits):
+        self.kwargs_q['nbits'] = nbits
+
+    def add_param(self, param_k, param_v):
+        self.kwargs_q[param_k] = param_v
+
+
+
+
